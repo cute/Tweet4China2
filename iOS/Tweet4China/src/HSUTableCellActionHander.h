5b9ba14eb568f8b782fc0a587d7b28bcd06a13c9
Jason Hsu <xuke@diandian.com>
2013-03-10 23:05:32 +0800

    update TableView Framework

---
+++ iOS/Tweet4China/src/HSUTableCellActionHander.h
@@ -0,0 +1,13 @@
+//
+//  HSUTableCellActionHander.h
+//  Tweet4China
+//
+//  Created by Jason Hsu on 3/10/13.
+//  Copyright (c) 2013 Jason Hsu <support@tuoxie.me>. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HSUTableCellActionHander : NSObject
+
+@end
---
+++ iOS/Tweet4China/src/HSUTableCellActionHander.m
@@ -0,0 +1,13 @@
+//
+//  HSUTableCellActionHander.m
+//  Tweet4China
+//
+//  Created by Jason Hsu on 3/10/13.
+//  Copyright (c) 2013 Jason Hsu <support@tuoxie.me>. All rights reserved.
+//
+
+#import "HSUTableCellActionHander.h"
+
+@implementation HSUTableCellActionHander
+
+@end
