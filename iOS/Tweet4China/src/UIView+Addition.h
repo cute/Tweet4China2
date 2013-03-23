698832a6dfc9c22e5bc4894380acede8157f329c
Jason Hsu <xuke@diandian.com>
2013-03-24 02:39:20 +0800

    caculate sub views frame manual instead of autolayout, now i can scroll smoothly

---
+++ iOS/Tweet4China/src/UIView+Addition.h
@@ -0,0 +1,13 @@
+//
+//  UIView+Addition.h
+//  Tweet4China
+//
+//  Created by Jason Hsu on 3/24/13.
+//  Copyright (c) 2013 Jason Hsu <support@tuoxie.me>. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Addition)
+
+@end
---
+++ iOS/Tweet4China/src/UIView+Addition.m
@@ -0,0 +1,13 @@
+//
+//  UIView+Addition.m
+//  Tweet4China
+//
+//  Created by Jason Hsu on 3/24/13.
+//  Copyright (c) 2013 Jason Hsu <support@tuoxie.me>. All rights reserved.
+//
+
+#import "UIView+Addition.h"
+
+@implementation UIView (Addition)
+
+@end
